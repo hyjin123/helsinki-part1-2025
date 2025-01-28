64b7de38ed7746159746c800d3a1d36c792e1f0f
hyjin123 <seanhoyeonjin@gmail.com>
2025-01-28 18:21:50 -0500

    exercise 1.9

--- unicafe/src/Components/Statistics.jsx
+++ unicafe/src/Components/Statistics.jsx
@@ -8,17 +8,21 @@ const Statistics = ({ good, neutral, bad }) => {
   //calculate the percentage of feedback
   const percentage = good / total;
 
-  return (
-    <div>
-      <h1>statistics</h1>
-      <p>good: {good}</p>
-      <p>neutral: {neutral}</p>
-      <p>bad: {bad}</p>
-      <p>all: {total}</p>
-      <p>average: {average}</p>
-      <p>positive: {percentage * 100}%</p>
-    </div>
-  );
+  if (total === 0) {
+    return <p>No feedback given</p>;
+  } else {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>good: {good}</p>
+        <p>neutral: {neutral}</p>
+        <p>bad: {bad}</p>
+        <p>all: {total}</p>
+        <p>average: {average}</p>
+        <p>positive: {percentage * 100}%</p>
+      </div>
+    );
+  }
 };
 
 export default Statistics;

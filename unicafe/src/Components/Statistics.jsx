33af321a945e4109366488ff883b6ca81d256a9d
hyjin123 <seanhoyeonjin@gmail.com>
2025-01-28 18:29:40 -0500

    exercise 1.10

--- unicafe/src/Components/Statistics.jsx
+++ unicafe/src/Components/Statistics.jsx
@@ -1,3 +1,5 @@
+import StatisticLine from "./StatisticLine";
+
 const Statistics = ({ good, neutral, bad }) => {
   //calculate the total votes
   const total = good + neutral + bad;
@@ -14,12 +16,12 @@ const Statistics = ({ good, neutral, bad }) => {
     return (
       <div>
         <h1>statistics</h1>
-        <p>good: {good}</p>
-        <p>neutral: {neutral}</p>
-        <p>bad: {bad}</p>
-        <p>all: {total}</p>
-        <p>average: {average}</p>
-        <p>positive: {percentage * 100}%</p>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={total} />
+        <StatisticLine text="average" value={average} />
+        <StatisticLine text="percentage" value={percentage + "%"} />
       </div>
     );
   }

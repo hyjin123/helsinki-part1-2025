e976c5c8e9843b46ce3726a019a9287dd0a1feae
hyjin123 <seanhoyeonjin@gmail.com>
2025-01-28 18:04:39 -0500

    exercise 1.7

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -23,6 +23,15 @@ const App = () => {
     setBad(bad + 1);
   };
 
+  //calculate the total votes
+  const total = good + neutral + bad;
+
+  //calculate the average score
+  const average = (good * 1 + neutral * 0 + bad * -1) / 9;
+
+  //calculate the percentage of feedback
+  const percentage = good / total;
+
   return (
     <div>
       <Header />
@@ -34,6 +43,9 @@ const App = () => {
       <p>good: {good}</p>
       <p>neutral: {neutral}</p>
       <p>bad: {bad}</p>
+      <p>all: {total}</p>
+      <p>average: {average}</p>
+      <p>positive: {percentage * 100}%</p>
     </div>
   );
 };
